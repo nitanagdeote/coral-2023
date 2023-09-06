4bccca15083574e4f3b984918c882287a5b27bf3
Nita <nitanagdeote1@gmail.com>
2023-09-06 04:23:50 -0400

    Create App.tsx
    App file

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as d3 from "d3";
+import Map from "./components/Map";
+
+const App = () => {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+};
+
+export default App;
